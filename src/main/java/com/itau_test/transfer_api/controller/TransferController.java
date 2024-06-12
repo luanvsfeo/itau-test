2fc9a143cf50812d12f146f9aeb0f24f6a548b1c
Luan Feo <luanfeo@gmail.com>
2024-06-12 14:44:01 -0300

    init commit

---
+++ src/main/java/com/itau_test/transfer_api/controller/TransferController.java
@@ -0,0 +1,22 @@
+package com.itau_test.transfer_api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TransferController {
+
+
+    @PostMapping
+    void create(){
+        // cria a transferencia
+
+        /*
+
+
+
+
+         */
+    }
+}
